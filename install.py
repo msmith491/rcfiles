e3adbd3e5f8a851b2d512adac31ae80eb844f943
Matt Smith <matthew.smith491@gmail.com>
2015-10-26 10:20:42 -0700

    Allowing exclusions via an --exclude flag in install.py

--- install.py
+++ install.py
@@ -5,17 +5,28 @@ from __future__ import (unicode_literals, print_function, division,
 
 import os
 import re
+import argparse
 
 EXCLUDE = ["README.md", ".gitignore", "LICENSE", ".git", "install.py"]
+DEFAULT_RE_EXCLUDE = [".*\.swp"]
 
-RE_EXCLUDE = [".*\.swp"]
+parser = argparse.ArgumentParser()
+parser.add_argument("--exclude", action="store", nargs="+",
+                    help="Accepts a list of regex strings used to "
+                    "exclude files from installing")
+args = parser.parse_args()
+
+if args.exclude:
+    re_exclude = [elem for elem in args.exclude] + DEFAULT_RE_EXCLUDE
+else:
+    re_exclude = DEFAULT_RE_EXCLUDE
 
 conf_files = ["/".join((os.path.dirname(os.path.abspath(__file__)), elem))
               for elem in os.listdir(".") if elem not in EXCLUDE]
 
 filtered = []
 for elem in conf_files:
-    if not any([re.match(regex, elem) for regex in RE_EXCLUDE]):
+    if not any([re.match(regex, elem) for regex in re_exclude]):
         filtered.append(elem)
 
 for f in filtered:
