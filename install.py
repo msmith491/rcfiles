3a20d8f9f42abd7a505cd6a18554130038171212
Matt Smith <matthew.smith491@gmail.com>
2015-10-25 22:58:10 -0700

    A few changes to install.py and zshrc

--- install.py
+++ install.py
@@ -4,11 +4,20 @@ from __future__ import (unicode_literals, print_function, division,
                         absolute_import)
 
 import os
+import re
 
 EXCLUDE = ["README.md", ".gitignore", "LICENSE", ".git", "install.py"]
 
-conf_files = [elem for elem in os.listdir(".") if elem not in EXCLUDE]
+RE_EXCLUDE = [".*\.swp"]
 
-for f in conf_files:
-    #os.symlink(f, "/".join((os.environ["HOME"], "." + f))
-    print(f, "/".join((os.environ["HOME"], "." + f)))
+conf_files = ["/".join((os.path.dirname(os.path.abspath(__file__)), elem))
+              for elem in os.listdir(".") if elem not in EXCLUDE]
+
+filtered = []
+for elem in conf_files:
+    if not any([re.match(regex, elem) for regex in RE_EXCLUDE]):
+        filtered.append(elem)
+
+for f in filtered:
+    print(f, "/".join((os.environ["HOME"] + "." + os.path.basename(f))))
+    # os.symlink(f, "/".join((os.environ["HOME"] + "." + os.path.basename(f))))
